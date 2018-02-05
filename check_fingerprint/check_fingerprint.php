12c385c0fdbe59cabda6bd311dcbb3be06dc2c6a
pp <pavel.pronskiy@gmail.com>
2018-02-05 15:37:42 +0300

    chronoforms5 widget

---
+++ check_fingerprint/check_fingerprint.php
@@ -0,0 +1,48 @@
+<?php
+/**
+* ChronoCMS version 1.0
+* Copyright (c) 2012 ChronoCMS.com, All rights reserved.
+* Author: (ChronoCMS.com Team)
+* license: Please read LICENSE.txt
+* Visit http://www.ChronoCMS.com for regular updates and information.
+**/
+namespace GCore\Admin\Extensions\Chronoforms\Actions\CheckFingerprint;
+/* @copyright:ChronoEngine.com @license:GPLv2 */defined('_JEXEC') or die('Restricted access');
+defined("GCORE_SITE") or die;
+Class CheckFingerprint extends \GCore\Admin\Extensions\Chronoforms\Action{
+	static $title = 'Check Fingerprint';
+	//static $setup = array('simple' => array('title' => 'Captcha'));
+	static $group = array('anti_spam' => 'Anti Spam');
+	var $events = array('success' => 0, 'fail' => 0);
+	var $defaults = array(
+		'error' => "Fingerprint check failed.",
+	);
+
+	function execute(&$form, $action_id){
+		$config =  $form->actions_config[$action_id];
+		$config = new \GCore\Libs\Parameter($config);
+
+		if (
+			isset($_COOKIE['formUserHash']) &&
+			isset($form->data['fp']) &&
+			$form->data['fp'] == $_COOKIE['formUserHash']
+		)
+		{
+			$this->events['success'] = 1;
+			$form->debug[$action_id][self::$title][] = "Fingerprint check passed.";
+			return true;
+		}
+		else
+		{
+			$this->events['fail'] = 1;
+			$form->errors['chrono_fingerprint'] = $config->get('error', "Fingerprint check failed.");
+			$form->debug[$action_id][self::$title][] = "Invalid fingerprint";
+			return false;
+		}
+	}
+
+	public static function config(){
+		echo \GCore\Helpers\Html::formStart('action_config check_fingerprint_action_config', 'check_fingerprint_action_config__XNX_');
+		echo \GCore\Helpers\Html::formEnd();
+	}
+}
---
+++ load_fingerprint/load_fingerprint.php
@@ -0,0 +1,55 @@
+<?php
+/**
+* ChronoCMS version 1.0
+* Copyright (c) 2012 ChronoCMS.com, All rights reserved.
+* Author: (ChronoCMS.com Team)
+* license: Please read LICENSE.txt
+* Visit http://www.ChronoCMS.com for regular updates and information.
+**/
+namespace GCore\Admin\Extensions\Chronoforms\Actions\LoadFingerprint;
+/* @copyright:ChronoEngine.com @license:GPLv2 */defined('_JEXEC') or die('Restricted access');
+defined("GCORE_SITE") or die;
+Class LoadFingerprint extends \GCore\Admin\Extensions\Chronoforms\Action{
+	static $title = 'Load Fingerprint';
+	static $group = array('anti_spam' => 'Anti Spam');
+
+	function execute(&$form, $action_id){
+		$config =  $form->actions_config[$action_id];
+		$config = new \GCore\Libs\Parameter($config);
+
+		$doc = \GCore\Libs\Document::getInstance();
+
+		$field_fp = \GCore\Helpers\Html::input('fp', array('type' => 'hidden', 'value' => ''));
+
+		$doc->addJsFile(\GCore\C::ext_url('chronoforms', 'admin').'actions/load_fingerprint/js/fingerprint2.min.js');
+		$doc->addJsFile(\GCore\C::ext_url('chronoforms', 'admin').'actions/load_fingerprint/js/jquery.cookie.js');
+		$doc->addJsCode('
+		jQuery(document).ready(function($){
+
+			var fp_options = {
+				excludeWebGL: true,
+				excludeSessionStorage: true,
+				excludeFlashFonts: true,
+				excludeJsFonts: true
+			};
+
+			var cookie_options = {
+				expires: 1
+			};
+			new Fingerprint2(fp_options).get(function(hash){
+				if ($.cookie("formUserHash") != hash)
+					$.cookie("formUserHash", hash, cookie_options);
+
+				$("#chronoform-'.$form->form['Form']['title'].'").append(\''.$field_fp.'\');
+				$("input#fp").val($.cookie("formUserHash"));
+			});
+
+		});
+		');
+	}
+
+	public static function config(){
+		echo \GCore\Helpers\Html::formStart('action_config load_fingerprint_action_config', 'load_fingerprint_action_config__XNX_');
+		echo \GCore\Helpers\Html::formEnd();
+	}
+}
